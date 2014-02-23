2ea42dfc581e9ee45178aa5a82b9cbfcc6bbd863
Sandor Weisz <sandor@gmail.com>
2014-02-23 10:38:04 -0600

    Added JS to get Farmers Market JSON without jQuery.

--- js/global.js
+++ js/global.js
@@ -7,5 +7,24 @@ document.addEventListener('DOMContentLoaded', function(){
     var startingZoom = 13;
 
     var map = L.mapbox.map(mapID, mapBoxID)
-        .setView(startingLatLong, 13);
+        .setView([41.888569, -87.635528], 13);
 });
+
+// Equivalent to $.getJSON
+request = new XMLHttpRequest();
+request.open('GET', 'http://data.cityofchicago.org/resource/i8y3-ytj4.json', true);
+
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400){
+    // Success!
+    data = JSON.parse(request.responseText);
+  } else {
+    // We reached our target server, but it returned an error
+  }
+};
+
+request.onerror = function() {
+  // There was a connection error of some sort
+};
+
+request.send();
