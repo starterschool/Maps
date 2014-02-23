7a8a9c77e3f3a77c02eea6602c4f867cda2db67b
Sandor Weisz <sandor@gmail.com>
2014-02-23 10:29:32 -0600

    Abstracted latlong & zoom to variables.

--- js/global.js
+++ js/global.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function(){
     var mapID = "map";
     var mapBoxID = "examples.map-9ijuk24y";
 
+    var startingLatLong = [41.888569, -87.635528];
+    var startingZoom = 13;
+
     var map = L.mapbox.map(mapID, mapBoxID)
-        .setView([41.888569, -87.635528], 13);
+        .setView(startingLatLong, 13);
 });
