0ba8f9efb525bc8e3e5d02de939ad850bd244cf9
Sandor Weisz <sandor@gmail.com>
2014-02-23 10:56:12 -0600

    Pseudo-code.

--- js/global.js
+++ js/global.js
@@ -21,6 +21,10 @@ request.onload = function() {
   if (request.status >= 200 && request.status < 400){
     // Success!
     var data = JSON.parse(request.responseText);
+
+    // Loop through the data
+        // … and update the map
+
   } else {
     // We reached our target server, but it returned an error
   }
