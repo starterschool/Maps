6e17bf72e4117b4c5f062ce471561ab9de19bfd6
Sandor Weisz <santheo@mac.com>
2014-02-25 10:01:05 -0600

    Lots of helpful commenting.

--- js/global.js
+++ js/global.js
@@ -1,18 +1,48 @@
-// Load the following when page is ready (sans jQuery)
+// - - - - - - -
+// METHOD FOR CALCULATING AS-CROW-FLIES DISTANCE BETWEEN TWO POINTS
+// - - - - - - -
+
+window.distance = function(start, end) {
+    // Assumes start & end are in [lat, long] format
+    return Math.sqrt(Math.pow(end[0] - start[0], 2) + Math.pow(end[1] - start[1], 2));
+};
+
+// - - - - - - -
+// LOAD THE FOLLOWING WHEN THE DOCUMENT IS READY
+// - - - - - - -
+
 document.addEventListener('DOMContentLoaded', function(){
+
+    // - - - - - - -
+    // MAP CONSTANTS
+    // - - - - - - -
+
     var mapID = "map";
     var mapBoxID = "examples.map-9ijuk24y";
-
     var startingLatLong = [41.888569, -87.635528];
     var startingZoom = 13;
 
-    var myGeoControl = L.mapbox.geocoderControl(mapBoxID);
+    // - - - - - - -
+    // WINDOW-SCOPED ARRAY FOR MARKET DATA
+    // - - - - - - -
+
     window.marketData = [];
 
-    // Initialize the map.
+    // - - - - - - -
+    // MAPBOX OBJECTS
+    // - - - - - - -
+
+    // Geocoder
+    var myGeoControl = L.mapbox.geocoderControl(mapBoxID);
+
+    // Map
     var map = L.mapbox.map(mapID, mapBoxID)
         .setView(startingLatLong, startingZoom)
         .addControl(myGeoControl);
+
+    // - - - - - - -
+    // HANDLE EVENT WHEN ADDRESS IS SUCCESFULLY MATCHED IN GEOCODER
+    // - - - - - - -
 
     myGeoControl.on('found', function(theResult) {
         // console.log(theResult);
@@ -27,57 +57,49 @@ document.addEventListener('DOMContentLoaded', function(){
         console.log(newList[0]);
     });
 
-    // Get JSON data from Data Portal. (The following is instead of $.getJSON)
+    // - - - - - - -
+    // AJAX REQUEST FOR FARMERS MARKET DATA
     // Source: https://data.cityofchicago.org/Environment-Sustainable-Development/Farmers-Markets-2013/i8y3-ytj4
+    // - - - - - - -
+
+    var dataPortalURL = 'http://data.cityofchicago.org/resource/i8y3-ytj4.json';
 
     request = new XMLHttpRequest();
-    request.open('GET', 'http://data.cityofchicago.org/resource/i8y3-ytj4.json', true);
+    request.open('GET', dataPortalURL, true);
 
     request.onload = function() {
-      if (request.status >= 200 && request.status < 400){
-        marketData = JSON.parse(request.responseText);
+        if (request.status >= 200 && request.status < 400){
+            marketData = JSON.parse(request.responseText);
 
-        // Loop through the data
-            // … and add a point for each market
+            // - - - - - - -
+            // LOOP THROUGH DATA
+            // For each market, add a marker to the map
+            // See http://mapbox.com/developers/simplestyle/ for marker style docs
+            // - - - - - - -
 
-        _.each(marketData, function(element, index, list) {
-            // console.log(element.latitude, element.longitude);
-            // … and update the map
-
-            L.mapbox.featureLayer({
-                // this feature is in the GeoJSON format: see geojson.org
-                // for the full specification
-                type: 'Feature',
-                geometry: {
-                    type: 'Point',
-                    // coordinates here are in longitude, latitude order because
-                    // x, y is the standard for GeoJSON and many formats
-                    coordinates: [element.longitude, element.latitude]
-                },
-                properties: {
-                    title: 'A Single Marker',
-                    description: 'Just one of me',
-                    // one can customize markers by adding simplestyle properties
-                    // http://mapbox.com/developers/simplestyle/
-                    'marker-size': 'large',
-                    'marker-color': '#f0a'
-                }
-            }).addTo(map);
-        });
-
-      } else {
-        // We reached our target server, but it returned an error
+            _.each(marketData, function(element, index) {
+                L.mapbox.featureLayer({
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [element.longitude, element.latitude]
+                    },
+                    properties: {
+                        title: 'A Single Marker',
+                        description: 'Just one of me',
+                        'marker-size': 'large',
+                        'marker-color': '#f0a'
+                    }
+                }).addTo(map);
+            });
+        } else {
+          // We reached our target server, but it returned an error
       }
     };
 
     request.onerror = function() {
-      // There was a connection error of some sort
+        // There was a connection error of some sort
     };
 
     request.send();
 });
-
-window.distance = function(start, end) {
-    // Assumes start & end are in [lat, long] format
-    return Math.sqrt(Math.pow(end[0] - start[0], 2) + Math.pow(end[1] - start[1], 2));
-};
